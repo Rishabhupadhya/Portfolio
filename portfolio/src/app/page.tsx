52ca936368c42012cfe8f77e58bc070035d2c08f
Rishabh Upadhyay <uprishabh29@gmail.com>
2026-01-26 00:07:41 +0530

    fixed error while build

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,10 +1,10 @@
-import Header from "@/components/Header";
-import ScrollProgress from "@/components/ScrollProgress";
-import HeroSection from "@/components/HeroSection";
-import AboutSection from "@/components/AboutSection";
-import ProjectsSection from "@/components/ProjectsSection";
-import ContactSection from "@/components/ContactSection";
-import BackgroundGlow from "@/components/BackgroundGlow";
+import Header from "../components/Header";
+import ScrollProgress from "../components/ScrollProgress";
+import HeroSection from "../components/HeroSection";
+import AboutSection from "../components/AboutSection";
+import ProjectsSection from "../components/ProjectsSection";
+import ContactSection from "../components/ContactSection";
+import BackgroundGlow from "../components/BackgroundGlow";
 
 export default function Home() {
   return (
